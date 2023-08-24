432953335cda1d75973c694fcc05bcbe6cc9889a
Javier-cnc <grumpyjg95@gmail.com>
2023-08-24 15:27:00 -0400

    change project  name

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -4,9 +4,7 @@ import { AppComponent } from './app.component';
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [
-        AppComponent
-      ],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -16,16 +14,18 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'starting-project'`, () => {
+  it(`should have as title 'multi-step-form-main'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('starting-project');
+    expect(app.title).toEqual('multi-step-form-main');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('starting-project app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain(
+      'multi-step-form-main app is running!'
+    );
   });
 });
