54df405538c1927795714ae5429bcbff81a49b72
Javier-cnc <grumpyjg95@gmail.com>
2023-08-26 17:13:41 -0400

    complete add-ons page

--- src/app/components/adds-page/adds-page.component.ts
+++ src/app/components/adds-page/adds-page.component.ts
@@ -12,14 +12,37 @@ import { ApplicationBackgroundService } from 'src/app/services/application-backg
 export class AddsPageComponent implements IPage {
   private terminated: ReplaySubject<void> = new ReplaySubject<void>();
 
-  validate(): boolean {
-    // TODO: ....
-    return true;
-  }
-  terminate(): void {
-    // TODO: ...
-    this.terminated.next();
-  }
+  // define the available options for the adds to be selected
+  options: {
+    name: string;
+    selected: boolean;
+    nameText: string;
+    description: string;
+    price: number;
+  }[] = [
+    {
+      name: 'onlineService',
+      selected: false,
+      nameText: 'Online service',
+      description: 'Access to multiplayer games',
+      price: 1,
+    },
+    {
+      name: 'largerStorage',
+      selected: false,
+      nameText: 'Larger storage',
+      description: 'Extra 1TB of cloud save',
+      price: 2,
+    },
+    {
+      name: 'customizableProfile',
+      selected: false,
+      nameText: 'Customizable profile',
+      description: 'Custom theme on your profile',
+      price: 2,
+    },
+  ];
+
   get Terminated(): Observable<void> {
     return this.terminated;
   }
@@ -28,5 +51,18 @@ export class AddsPageComponent implements IPage {
     appService.CurrentPage = this;
     appService.CurrentPageIndex =
       urls.navigationSequence.addsPage.sequenceNumber;
+  }
+
+  validate(): boolean {
+    // TODO: ....
+    return true;
+  }
+  terminate(): void {
+    // TODO: ...
+    this.terminated.next();
+  }
+
+  test(event: any) {
+    event.preventDefault();
   }
 }
