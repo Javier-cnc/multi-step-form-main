052fa977e5eb9904d6dcf601c8a01c862a9da399
Javier-cnc <grumpyjg95@gmail.com>
2023-08-26 16:19:59 -0400

    add styling rules and behavior to plans plage

--- src/app/components/plan-page/plan-page.component.ts
+++ src/app/components/plan-page/plan-page.component.ts
@@ -12,6 +12,36 @@ import { ApplicationBackgroundService } from 'src/app/services/application-backg
 export class PlanPageComponent implements IPage {
   private terminated: ReplaySubject<void> = new ReplaySubject<void>();
 
+  isYearly: boolean = false;
+
+  selectedPlan: string = '';
+
+  options: {
+    name: string;
+    price: number;
+    iconAddress: string;
+    freeMonthQuantity: number;
+  }[] = [
+    {
+      name: 'Arcade',
+      price: 9,
+      iconAddress: 'assets/images/icon-arcade.svg',
+      freeMonthQuantity: 2,
+    },
+    {
+      name: 'Advanced',
+      price: 12,
+      iconAddress: 'assets/images/icon-advanced.svg',
+      freeMonthQuantity: 2,
+    },
+    {
+      name: 'Pro',
+      price: 15,
+      iconAddress: 'assets/images/icon-pro.svg',
+      freeMonthQuantity: 2,
+    },
+  ];
+
   get Terminated(): Observable<void> {
     return this.terminated;
   }
@@ -23,6 +53,9 @@ export class PlanPageComponent implements IPage {
     // notifies the rest of the appliation which one is the index in the navigation sequence of the current page
     this.appService.CurrentPageIndex =
       urls.navigationSequence.planPage.sequenceNumber;
+
+    // apply a default plan
+    this.selectedPlan = this.options[0].name;
   }
 
   terminate(): void {
